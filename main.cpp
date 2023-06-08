0ca8ba821c37eb539aa2f1ff8982bd4df167cc1b
SeyiAkinwale <72584469+SeyiAkinwale@users.noreply.github.com>
2023-06-08 17:26:09 +0000

    Node class

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
 
+class Node
+{
+  public:
+    std::string val;
+    Node * next;
+
+  Node(std::string initialVal)
+  {
+    val = initialVal;
+    next = nullptr;
+  }
+};
+
 int main() {
   std::cout << "Hello World!\n";
 }
