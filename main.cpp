9ccf319adf0dd6f18db4a8874fbb0232bb5e4196
SeyiAkinwale <72584469+SeyiAkinwale@users.noreply.github.com>
2023-06-09 14:46:58 +0000

    handling empty list

--- main.cpp
+++ main.cpp
@@ -15,34 +15,19 @@ class Node
 
 Node * reverseList(Node * head)
 {
+  if(head = nullptr) return nullptr;
   Node * prev = nullptr;
   Node * curr = head;
   Node * back = head->next;
 
   while (curr != nullptr)
-  {
-    if(prev == nullptr && (back == nullptr))
-    {
-      prev = curr;
-      curr = nullptr;
-      std::cout<<std::endl<<"hi1";
-    }
-
-    if (prev == nullptr && (back != nullptr))
-    {
-      curr->next = nullptr;
-      prev = curr;
-      curr = back;
-      back = back->next;
-      std::cout<<std::endl<<"hi2";
-    }
-    
+  {   
     if (prev !=nullptr && (curr != nullptr))
     {
       curr->next = prev;
       prev = curr;
       curr = back;
-      back = back->next;
+ //     back = back->next;
       std::cout<<std::endl<<"hi3";
     }
   std::cout<<std::endl<<prev->val;
@@ -52,16 +37,18 @@ Node * reverseList(Node * head)
 }
 int main() 
 {
+  Node * cute = nullptr;
   Node a("a");
   Node b("b");
   Node c("c");
   Node d("d");
 
-//  a.next = &b;
+  a.next = &b;
 //  b.next = &c;
 //  c.next = &d;
 
 //  std::cout<<"b "<<b.val<<std::endl;
-  Node * end =reverseList(&a);
-//  std::cout <<"end "<<end->val;
+  Node * end =reverseList(cute);
+  if(end == nullptr) cout<<"nullptr";
+//  std::cout <<std::endl<<"end "<<end->val;
 }
