dc31eeabeead24c926ac60e4a783e69a415fcfc5
SeyiAkinwale <72584469+SeyiAkinwale@users.noreply.github.com>
2023-06-12 22:52:59 +0000

    abc worked

--- .ccls-cache/@home@runner@reverseList/main.cpp
+++ .ccls-cache/@home@runner@reverseList/main.cpp
@@ -13,6 +13,17 @@ class Node
   }
 };
 
+void printList(Node * head)
+{
+  std::cout<<std::endl<<"printList ";
+  if(head==nullptr) std::cout<<std::endl<<"empty list ";
+  Node * curr = head;
+  while (curr!=nullptr)
+  {
+    std::cout <<std::endl<<curr->val;
+    curr=curr->next;
+  }
+}
 Node * reverseList(Node * head)
 {
   if(head = nullptr) return nullptr;
@@ -22,15 +33,40 @@ Node * reverseList(Node * head)
 
   while (curr != nullptr)
   {   
-    if (prev !=nullptr && (curr != nullptr))
+    if (prev == nullptr && (back == nullptr)) //singleton
+    {
+      prev = curr;
+      curr = nullptr;
+      std::cout<<std::endl<<"hi1";
+    }
+
+    if (prev == nullptr && (back != nullptr)) //first of 2 elems
+    {
+      curr->next = nullptr;
+      prev = curr;
+      curr = back;
+      back = back->next;
+      std::cout<<std::endl<<"hi2";
+    }
+
+    if (prev != nullptr && (back ==nullptr)) //second of 2 elems
     {
       curr->next = prev;
       prev = curr;
       curr = back;
- //     back = back->next;
+      back = back->next;
       std::cout<<std::endl<<"hi3";
     }
+  //  if (prev !=nullptr && (curr != nullptr))
+  //  {
+ //     curr->next = prev;
+//      prev = curr;
+//      curr = back;
+ //     back = back->next;
+//      std::cout<<std::endl<<"hi4";
+//    }
   std::cout<<std::endl<<prev->val;
+  std::cout<<std::endl<<"watch";
   }
 
   return prev;
@@ -43,12 +79,22 @@ int main()
   Node c("c");
   Node d("d");
 
-  a.next = &b;
+//  a.next = &b;
 //  b.next = &c;
 //  c.next = &d;
 
 //  std::cout<<"b "<<b.val<<std::endl;
-  Node * end =reverseList(cute);
-  if(end == nullptr) cout<<"nullptr";
-//  std::cout <<std::endl<<"end "<<end->val;
+
+  Node end("end");
+  Node dummy("dummy");
+  printList(&a);
+  end.next=reverseList(&a);
+  
+  if(end.next == nullptr)std::cout<<std::endl<<"nullptr";
+  else std::cout <<std::endl<<"end"<<end.val;
+  printList(&a);
+
+//  std::cout<<endl<<b.val;
+//  std::cout<<endl<<a.val;
+ 
 }
--- main.cpp
+++ main.cpp
@@ -13,24 +13,62 @@ class Node
   }
 };
 
+void printList(Node * head)
+{
+  std::cout<<std::endl<<"printList ";
+  if(head==nullptr) std::cout<<std::endl<<"empty list ";
+  Node * curr = head;
+  while (curr!=nullptr)
+  {
+    std::cout <<std::endl<<curr->val;
+    curr=curr->next;
+  }
+}
 Node * reverseList(Node * head)
 {
-  if(head = nullptr) return nullptr;
+
+  if(head == nullptr) return nullptr;
   Node * prev = nullptr;
   Node * curr = head;
   Node * back = head->next;
 
   while (curr != nullptr)
-  {   
-    if (prev !=nullptr && (curr != nullptr))
+  { 
+      std::cout<<"watch";
+    if (prev == nullptr && (back == nullptr)) //singleton
+    {
+      prev = curr;
+      curr = nullptr;
+      std::cout<<std::endl<<"hi1";
+    }
+
+    else if (prev == nullptr && (back != nullptr)) //first of 2 elems
+    {
+      curr->next = nullptr;
+      prev = curr;
+      curr = back;
+      back = back->next;
+      std::cout<<std::endl<<"hi2";
+    }
+
+    else if (prev != nullptr ) //second of 2 elems
     {
       curr->next = prev;
       prev = curr;
       curr = back;
- //     back = back->next;
+      back = nullptr;
       std::cout<<std::endl<<"hi3";
     }
+  //  if (prev !=nullptr && (curr != nullptr))
+  //  {
+ //     curr->next = prev;
+//      prev = curr;
+//      curr = back;
+ //     back = back->next;
+//      std::cout<<std::endl<<"hi4";
+//    }
   std::cout<<std::endl<<prev->val;
+  std::cout<<std::endl<<"watch";
   }
 
   return prev;
@@ -44,11 +82,21 @@ int main()
   Node d("d");
 
   a.next = &b;
-//  b.next = &c;
-//  c.next = &d;
+  b.next = &c;
+  c.next = &d;
 
 //  std::cout<<"b "<<b.val<<std::endl;
-  Node * end =reverseList(cute);
-  if(end == nullptr) cout<<"nullptr";
-//  std::cout <<std::endl<<"end "<<end->val;
+
+  Node end("end");
+  Node dummy("dummy");
+  printList(&a);
+  end.next=reverseList(&a);
+  
+  if(end.next == nullptr)std::cout<<std::endl<<"nullptr";
+  else std::cout <<std::endl<<end.val;
+  printList(&d);
+
+//  std::cout<<endl<<b.val;
+//  std::cout<<endl<<a.val;
+ 
 }
